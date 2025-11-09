7e21f5b0e692ae62fd2ee17a0e03d2fdd7d301c6
Acedo, Mark Relan Gercee <125295018+mawrk19@users.noreply.github.com>
2025-11-09 13:21:12 +0800

    FEAT: Add additional allowed origins for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -35,6 +35,9 @@ return [
             'http://127.0.0.1:3000',
             'http://127.0.0.1:8080',
             'http://127.0.0.1:5173',
+            'capacitor://localhost',
+            'ionic://localhost',
+            'http://localhost',
         ],
     
     'allowed_origins_patterns' => [
